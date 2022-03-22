b552255762aef625ddc6aa3705b9ea10951646ab
rafaelnogalha <rafaelnogalha@gmail.com>
2022-03-22 16:18:45 -0300

    ListLenght module finished

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,10 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: sum(list, 0)
+
+  defp sum([], acc), do: acc
+
+  defp sum([head | tail] = list, acc) do
+    acc = acc + 1
+    sum(tail, acc)
   end
 end
